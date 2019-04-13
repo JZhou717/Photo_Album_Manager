ef5d65f7b6026179e8ec4e9f9f6e3195f941337f
JZhou717 <32233263+JZhou717@users.noreply.github.com>
2019-04-13 17:14:37 -0400

    Fixed search fxml

--- src/model/Search.java
+++ src/model/Search.java
@@ -12,19 +12,30 @@ import view.PhotosController;
 
 public class Search{
 
-	public static ObservableList<Photo> tag_search_results() {
+	public static ObservableList<Photo> tag_search_results(String type, String value) {
 		
 		ObservableList<Photo> ret = FXCollections.observableArrayList();
-		User current_user = PhotosController.get_admin().getUserByName(PhotosController.get_user());
-		ObservableList<Album> user_albums = current_user.getAlbums();
-		Album album;
 		
-		//Going through each album of the user
+		User current_user = PhotosController.get_admin().getUserByName(PhotosController.get_user());
+		Album album;
+		ObservableList<Album> user_albums = current_user.getAlbums();
+		Photo photo;
+		ObservableList<Tag> photo_tags;
+		Tag tag;
+		
+		//Going through each Album of the User
 		for(int i = 0; i < user_albums.size(); i++) {
 			album = user_albums.get(i);
-			//Going through each Photo in the album
+			//Going through each Photo in the Album
 			for(int j = 0; j < album.size(); j++) {
-				
+				photo = album.getPhotoAt(j);
+				photo_tags = photo.get_tags();
+				//Going through each Tag in the Photo
+				for(int k = 0; k < photo_tags.size(); k++) {
+					//See if there is a match on the tag type
+					tag = photo_tags.get(i);
+					
+				}
 			}
 		}
 		
